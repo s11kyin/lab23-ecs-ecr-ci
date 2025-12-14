e8b2693943c6b1cd2fc438477207a7649fd7ccc3
s11kyin <richiekyin@gmail.com>
2025-12-14 13:11:02 +0000

    file update

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-return f"""
-<html>
-  <head><title>Lab 22 - Green Version</title></head>
-  <body style="background-color:#e0ffe0;">
-    <h1>DevOps Easy Learning - Lab 22</h1>
-    <h2>GREEN deployment</h2>
-    <p>Served from container: {hostname}</p>
-  </body>
-</html>
-"""
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    hostname = socket.gethostname()
+    return f"""
+    <html>
+      <head><title>Lab 21 - ECS + ECR</title></head>
+      <body>
+        <h1>DevOps Easy Learning - Lab 21</h1>
+        <p>This response is served from container: {hostname}</p>
+      </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
