9c1f6d220616ac1eb28d2fc117d0a968ab75532c
s11kyin <richiekyin@gmail.com>
2025-12-14 15:07:12 +0000

    app.py update

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def hello():
     hostname = socket.gethostname()
     return f"""
     <html>
-      <head><title>Lab 21 - ECS + ECR</title></head>
+      <head><title>Lab 24 - ECS + ECR</title></head>
       <body>
-        <h1>DevOps Easy Learning - Lab 24</h1>
+        <h1>DevOps Easy Learning - s11kyin  - Lab 24</h1>
         <p>This response is served from container: {hostname}</p>
       </body>
     </html>
