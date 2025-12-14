b948121c17f6b47281b51475bdaa5e954ecbe12f
s11kyin <richiekyin@gmail.com>
2025-12-14 10:51:39 +0000

    Initial commit - lab 23 app

---
+++ app.py
@@ -0,0 +1,10 @@
+return f"""
+<html>
+  <head><title>Lab 22 - Green Version</title></head>
+  <body style="background-color:#e0ffe0;">
+    <h1>DevOps Easy Learning - Lab 22</h1>
+    <h2>GREEN deployment</h2>
+    <p>Served from container: {hostname}</p>
+  </body>
+</html>
+"""
